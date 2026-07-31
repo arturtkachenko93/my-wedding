abf3ea87a44b01644966297cd341b45b55abb490
Tkachenko Artur <atkachenko2@alfabank.ru>
2026-07-31 14:36:01 +0300

    some fixes

--- api/send-rsvp.js
+++ api/send-rsvp.js
@@ -57,7 +57,6 @@ function formatMessage(data) {
     let message = `💍 <b>Новый ответ на приглашение!</b>\n\n`;
     message += `${attendanceEmoji} <b>Статус:</b> ${attendanceText}\n`;
     message += `👤 <b>Имя:</b> ${data.firstName} ${data.lastName}\n`;
-    message += `📧 <b>Email:</b> ${data.email}\n`;
     message += `📱 <b>Телефон:</b> ${data.phone}\n`;
     
     // Проверяем, что drinks существует и не пуст
